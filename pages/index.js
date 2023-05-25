11fac32caaac156a56a756b9f499a86837882c68
Hamilton, Andrew (ah9667) <ah9667@att.com>
2023-05-25 11:08:06 -0500

    [learn] - pages commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import sleep from 'await-sleep'
+
+export default function Home(props) {
+    return (
+      <div>
+        <h2>Welcome to our homepage.</h2>
+        <p>This is the best home page in the world. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aspernatur illum architecto cumque recusandae fuga sequi necessitatibus deleniti repellat harum nobis, dolor veniam vero deserunt. Voluptatibus, ducimus deserunt. Recusandae, dolore.</p>
+        <p>The weather: {props.forecast}</p>
+      </div>
+    )
+  }
+  
+  //only want to use this if you need to REALLY needs to be dynamic unlike getstatisprops
+  export async function getServerSideProps() {
+    const response = await fetch("https://api.weather.gov/gridpoints/MFL/109,49/forecast")
+    const data = await response.json()
+  
+    await sleep(3000)
+
+    return {
+      props: {
+        forecast: data.properties.periods[0].detailedForecast
+      }
+    }
+  }
